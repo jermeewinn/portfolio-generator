54e8980cee08f5bb76c8e9361b6bd522c559e641
Jeremy Huynh <jeahuynh18@gmail.com>
2021-11-13 14:04:39 -0800

    added src and page-template.js

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-const generatePage = () => 'Name: Jane, Github: janehub';
-    console.log(generatePage());
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if(err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
 
 // Code from Module 9.1
 // const printProfileData = profileDataArr => {
