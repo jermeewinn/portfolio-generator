2913d3eb1a00842456a1d263ddfb2e65010bd920
Jeremy Huynh <jeahuynh18@gmail.com>
2021-11-14 12:15:56 -0800

    implemented inquirer from npm

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(name, github);
 
-const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if(err) throw err;
 
-const [name, github] = profileDataArgs;
-
-
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if(err) throw err;
-
-    console.log('Portfolio complete! Check out index.html to see the output!')
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// });
 
 // Code from Module 9.1
 // const printProfileData = profileDataArr => {
